795ca30b03582c28724cc93de078bc9a998d5957
angela172 <120313923+angela172@users.noreply.github.com>
2025-05-01 14:52:46 +0400

    Update __init__.py

--- database/__init__.py
+++ database/__init__.py
@@ -13,7 +13,7 @@ try:
     DB_USER = st.secrets["DB_USER"]
     DB_PASSWORD = st.secrets["DB_PASSWORD"]
     DB_NAME = st.secrets["DB_NAME"]
-    DB_HOST = st.secrets.get("DB_HOST", "localhost")  # fallback default
+    DB_HOST = st.secrets.get("DB_HOST", "103.173.18.175")  # fallback default
     DB_PORT = st.secrets.get("DB_PORT", 5432)         # fallback default
 except KeyError as e:
     raise RuntimeError(f"Missing key in Streamlit secrets: {e}")
