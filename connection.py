bd0164b4ccbdde922b9074ed83633075ecbbb703
angela172 <120313923+angela172@users.noreply.github.com>
2025-05-01 11:14:26 +0400

    Update connection.py

--- connection.py
+++ connection.py
@@ -2,18 +2,18 @@ import streamlit as st
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database.models import Base
-
+from database import DB_USER, DB_PASSWORD, DB_NAME, DB_HOST, DB_PORT
 # Create a cached database connection function
 @st.cache_resource
 def init_connection():
     """Initialize database connection. This function will be cached and only run once, unless invalidated"""
     try:
         # Database credentials for remote PostgreSQL server
-        DB_HOST = "103.173.18.175"
+        '''DB_HOST = "103.173.18.175"
         DB_PORT = 5432
         DB_NAME = "CustomerFeedback"
         DB_USERNAME = "postgres"  # You may need to update this
-        DB_PASSWORD = "Po$tgr3$18Sol"  # You may need to update this
+        DB_PASSWORD = "Po$tgr3$18Sol"  # You may need to update this'''
         
         # PostgreSQL connection string
         DATABASE_URL = f"postgresql+psycopg2://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
